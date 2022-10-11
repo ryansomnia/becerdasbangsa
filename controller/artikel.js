64182b06dacc00bb4111d773c7e931494c9398f7
ryansomnia <laskarimmanuel@gmail.com>
2022-10-11 12:35:39 +0700

    artikel yuhuu

--- controller/artikel.js
+++ controller/artikel.js
@@ -32,6 +32,37 @@ let artikel = {
            res.status(500).send(respon) 
            console.log(respon);
         }
+    },
+    insertData : async(req, res)=>{
+        try {
+            conn.query(`SELECT * FROM artikel`, (err,data, fields)=>{
+                if (err) {
+                    let respon = {
+                        code : 400,
+                        status : 'error',
+                        error : err       
+                    }
+                   res.status(400).send(respon) 
+                   console.log(respon);
+                } else {
+                    let respon = {
+                        code : 200,
+                        status : 'success',
+                        data : data       
+                    }
+                   res.status(200).send(respon) 
+                   console.log(respon);
+                }
+            })
+        } catch (error) {
+            let respon = {
+                code : 500,
+                status : 'error',
+                error : error       
+            }
+           res.status(500).send(respon) 
+           console.log(respon);
+        }
     }
 }
 
--- routes.js
+++ routes.js
@@ -2,6 +2,13 @@ const express = require('express');
 const artikel = require('./controller/artikel')
 const router = express.Router();
 
+
+// login
+// register
 router.get('/api/cbn/getAllData', artikel.getAllData)
+// get data news
+// get data artikel
+// get data event
+// regisppdb
 
 module.exports = router;
