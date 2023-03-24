b0a4b5c2fc789c23e0d18a831d8ba97fd8baa64b
70001511952020 <heriyanto.sitorus@brilife.co.id>
2023-03-24 17:44:56 +0700

    cors

--- index.js
+++ index.js
@@ -10,8 +10,12 @@ const PORT = process.env.PORT || 5000;
 const app = express()
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'OPTIONS, GET, PUT, PATCH, POST, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, X-Requested-With, Authorization');
     next();
   });
+
+
 // set body parser
 app.options('*', cors());
 app.use(express.static('public'));
